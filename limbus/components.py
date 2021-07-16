287d4e76910b91cd9240b332b6b4508d8eed5ef2
Luis Ferraz <luisferrazc@gmail.com>
2021-07-16 19:06:49 +0200

    added new components to create an small network with tensors and refactor of the core module

--- limbus/components.py
+++ limbus/components.py
@@ -1,6 +1,91 @@
-from typing import Any
+from typing import Any, List
+from pathlib import Path
+
+from matplotlib import pyplot as plt
+import numpy as np
+import PIL
+import torch
+import kornia
 
 from limbus.core import Component, ComponentState, Params
+
+
+class ImageReader(Component):
+    """Component that holds a constant.
+
+    Args:
+        name: component name.
+        value: path to an image or image folder.
+
+    """
+    def __init__(self, name: str, value: str):
+        super().__init__(name)
+        self._value: List[Path] = []
+        self._idx = 0
+        if Path(value).is_dir():
+            self._value = list(Path(value).glob("*"))
+        else:
+            self._value.append(Path(value))
+
+        self._outputs.declare("image")
+
+    def forward(self, inputs: Params) -> ComponentState:
+        while True:
+            if self._idx >= len(self._value):
+                return ComponentState.STOPPED
+            try:
+                image: torch.Tensor = kornia.image_to_tensor(np.asarray(PIL.Image.open(str(self._value[self._idx]))))
+                break
+            except:
+                pass
+            self._idx += 1
+        # images must be in the range [0, 1]
+        image = image.div(255.)
+        self._outputs.image = image.clamp(0, 1)
+        return ComponentState.OK
+
+
+class RGB2HLS(Component):
+    """Component to add two input and output the result."""
+    def __init__(self, name: str):
+        super().__init__(name)
+        self._inputs.declare("inp")
+        self._outputs.declare("out")
+
+    def forward(self, inputs: Params) -> ComponentState:
+        self._outputs.out = kornia.rgb_to_hls(inputs.inp)
+        return ComponentState.OK
+
+
+class Select(Component):
+    """Component to add two input and output the result."""
+    def __init__(self, name: str):
+        super().__init__(name)
+        self._inputs.declare("c")
+        self._inputs.declare("inp")
+        self._outputs.declare("out")
+
+    def forward(self, inputs: Params) -> ComponentState:
+        self._outputs.out = torch.select(inputs.inp, -3, inputs.c)
+        return ComponentState.OK
+
+
+class ImageShow(Component):
+    """Component to print the input in the console."""
+    def __init__(self, name: str):
+        super().__init__(name)
+        self._inputs.declare("image")
+
+    def forward(self, inputs: Params) -> ComponentState:
+        img = inputs.image
+        if img.dim() == 2:
+            img = img[None].repeat(3, 1, 1)
+        img: np.ndarray = kornia.tensor_to_image(
+            img.mul(255).clamp(0, 255).int())
+        plt.figure(self._name)
+        plt.imshow(img)
+        plt.show()
+        return ComponentState.OK
 
 
 class Constant(Component):
@@ -10,8 +95,8 @@ class Constant(Component):
         self._value = value
         self._outputs.declare("out")
 
-    def forward(self, inputs: Params, outputs: Params) -> ComponentState:
-        outputs.out = self._value
+    def forward(self, inputs: Params) -> ComponentState:
+        self._outputs.out = self._value
         return ComponentState.OK
 
 
@@ -21,7 +106,7 @@ class Printer(Component):
         super().__init__(name)
         self._inputs.declare("inp")
 
-    def forward(self, inputs: Params, outputs: Params) -> ComponentState:
+    def forward(self, inputs: Params) -> ComponentState:
         print(inputs.inp)
         return ComponentState.OK
 
@@ -34,6 +119,6 @@ class Adder(Component):
         self._inputs.declare("b")
         self._outputs.declare("sum_out")
 
-    def forward(self, inputs: Params, outputs: Params) -> ComponentState:
-        outputs.sum_out = inputs.a + inputs.b
+    def forward(self, inputs: Params) -> ComponentState:
+        self._outputs.sum_out = inputs.a + inputs.b
         return ComponentState.OK
--- limbus/core.py
+++ limbus/core.py
@@ -1,11 +1,13 @@
 from abc import abstractmethod
-from typing import Dict, Any, List
+from typing import Dict, Any, List, Collection
 from enum import Enum
+from dataclasses import dataclass
 
 import torch.nn as nn
 
 
 class ComponentState(Enum):
+    STOPPED = 0
     OK = 1
     NotImplemented = -1
 
@@ -23,7 +25,7 @@ class Params:
 
     def __getitem__(self, name: str) -> Any:
         return getattr(self, name)
-    
+
     def __repr__(self) -> str:
         return ''.join(
             (
@@ -57,81 +59,108 @@ class Component(nn.Module):
         return self._outputs
 
     @abstractmethod
-    def forward(self, inputs: Params, outputs: Params) -> ComponentState:
+    def forward(self, inputs: Params) -> ComponentState:
         return ComponentState.NotImplemented
 
 
+@dataclass
+class _Node:
+    type: NodeType
+    component: Component
+    inputs: Dict[Any, List]
+    outputs: Dict[Any, List]
+
+
+@dataclass
+class _Link:
+    node: str
+    pin: str
+
 class ComponentsManager(nn.Module):
     """Class to manage the Limbus Components.
-    
+
     It holds the logic to construct the pipeline to link components.
     """
     def __init__(self):
-        self.nodes: Dict[str, Dict[str, Any]]= {}
+        self.nodes: Dict[str, _Node]= {}
         self._seq = []
 
     def connect(self, _from: Component, _from_name: str, _to: Component, _to_name: str) -> None:
         """Method to connect one component output to another component input.
-        
+
         Args:
             _from: the origin component instance.
             _from_name: the origin component member name.
             _to: the destination component instance.
             _to_name: the destination component member name.
         """
-        for t in [_from, _to]:
-            if t.name not in self.nodes.keys():
-                if not len(t.inputs.__dict__.keys()) and not len(t.outputs.__dict__.keys()):
+        for comp in [_from, _to]:
+            if comp.name not in self.nodes.keys():
+                # check type of component and list all the input and output pins
+                inp_keys: Collection[Any] = comp.inputs.__dict__.keys()
+                out_keys: Collection[Any] = comp.outputs.__dict__.keys()
+                if not len(inp_keys) and not len(out_keys):
                     raise ValueError("No inputs and outputs provided!!!")
-                inp = {}
+
+                inp: Dict[Any, List] = {key: [] for key in inp_keys}
+                out: Dict[Any, List] = {key: [] for key in out_keys}
+
                 node_type = NodeType.Mid
-                for k in t.inputs.__dict__.keys():
-                    inp[k] = {}
-                if not len(t.inputs.__dict__.keys()):
+                if not len(inp_keys):
                     node_type = NodeType.Start
-                out = {}
-                for k in t.outputs.__dict__.keys():
-                    out[k] = {}
-                if not len(t.outputs.__dict__.keys()):
+                if not len(out_keys):
                     node_type = NodeType.End
-                self.nodes[t.name] = {"type": node_type, "inputs": inp, "outputs": out, "object": t}
+                # add the node to the graph
+                self.nodes[comp.name] = _Node(node_type, comp, inp, out)
 
-        if not self.nodes[_from.name]["outputs"][_from_name]:
-            self.nodes[_from.name]["outputs"][_from_name] = {_to.name: input}
-        else:
-            raise ValueError("Already setted!!!")
+        # set connections between nodes (they can be multiple)
+        self.nodes[_from.name].outputs[_from_name].append(_Link(_to.name, _to_name))
+        self.nodes[_to.name].inputs[_to_name].append(_Link(_from.name, _from_name))
 
-        if not self.nodes[_to.name]["inputs"][_to_name]:
-            self.nodes[_to.name]["inputs"][_to_name] = {_from.name: _from_name}
-        else:
-            raise ValueError("Already setted!!!")
+    def _traverse(self, node_name) -> None:
+        pins: Collection[List[_Link]] = self.nodes[node_name].outputs.values()
+        pin: List[_Link]
+        link: _Link
+        for pin in pins:
+            for link in pin:
+                node: _Node = self.nodes[link.node]
+                # check if all the components required by the input pins are added
+                inp_links: List[_Link]
+                for _, inp_links in node.inputs.items():
+                    # check that all the connections for that link are added
+                    for inp_link in inp_links:
+                        if inp_link.node not in self._seq:
+                            return
+                self._seq.append(link.node)
+                self._traverse(link.node)
 
-    def _traverse(self, k) -> List:
-        out = self.nodes[k]["outputs"].values()
-        for comp in out:
-            k = list(comp.keys())[0]
-            vs = self.nodes[k]
-            for _, inp in vs["inputs"].items():
-                if list(inp.keys())[0] not in self._seq:
-                    return
-            self._seq.append(k)
-            self._traverse(k)
-
-    def traverse(self):
+    def traverse(self) -> None:
         """Method to traverse the components graph before execution."""
         # find start nodes
         self._seq = []
 
-        for k, vs in self.nodes.items():
-            if vs["type"] == NodeType.Start:
-                self._seq.append(k)
-                self._traverse(k)
+        node_name: str
+        node: _Node
+        for node_name, node in self.nodes.items():
+            if node.type == NodeType.Start:
+                self._seq.append(node_name)
+                self._traverse(node_name)
 
-    def execute(self):
+    def execute(self) -> None:
         """Method to execute the components graph."""
-        for mod in self._seq:
-            obj = self.nodes[mod]["object"]
-            for k, v in self.nodes[mod]["inputs"].items():
-                obj.inputs.__dict__[k] = (
-                    self.nodes[list(v.keys())[0]]["object"].outputs.__dict__[list(v.values())[0]])
-            obj.forward(obj.inputs, obj.outputs)
+        for node_name in self._seq:
+            obj = self.nodes[node_name].component
+            pin: str
+            links: List[_Link]
+            # get values from the connected components
+            inputs = Params()
+            for pin, links in self.nodes[node_name].inputs.items():
+                # get input value for each pin
+                values = []  # for now we assume that params are passed as a list
+                for link in links:
+                    values.append(self.nodes[link.node].component.outputs[link.pin])
+                if len(links) == 1:
+                    inputs.declare(pin, values[0])
+                else:
+                    inputs.declare(pin, values)
+            obj.forward(inputs)
