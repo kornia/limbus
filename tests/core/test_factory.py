36ed1f47e2beac931a40be5f613987b3b663c8f8
Luis Ferraz <luisferrazc@gmail.com>
2022-02-15 20:37:10 +0100

    added some extra tests for the factory

--- tests/core/test_factory.py
+++ tests/core/test_factory.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 import torch
 
 import limbus.components
-from limbus.core import Component, ComponentState, Params, register_component
+from limbus.core import Component, ComponentState, Params
+from limbus import core
 
 
 def test_registry():
@@ -30,7 +33,7 @@ def test_registry():
             self._outputs.set_param("out", a - b)
             return ComponentState.OK
 
-    register_component(Subs, "test0.test1")
+    core.register_component(Subs, "test0.test1")
     subs = limbus.components.test0.test1.Subs("name")  # type: ignore
 
     inp = Params()
@@ -38,3 +41,85 @@ def test_registry():
     inp.declare("b", torch.Tensor, torch.randn(2, 3))
     subs(inp)
     assert subs.outputs.get_param("out").shape == torch.Size([2, 3])
+
+
+def test_registry_from_yml_with_args(tmpdir_factory):
+    fn = str(Path(tmpdir_factory.mktemp("test_registry_from_yml")) / "test.yml")
+    with open(fn, "w") as f:
+        f.write("""
+            torch.unsqueeze:
+                params: {input: torch.Tensor, dim: int}
+                returns: {out: torch.Tensor}
+    """)
+    core.register_components_from_yml(str(fn))
+    comp = limbus.components.torch.unsqueeze("test")
+    inp = Params()
+    inp.declare("input", torch.Tensor, torch.randn(2, 3))
+    inp.declare("dim", int, 2)
+    comp(inp)
+    assert comp.outputs.get_param("out").shape == torch.Size([2, 3, 1])
+
+
+def test_registry_from_yml_with_args_changing_output_name(tmpdir_factory):
+    fn = str(Path(tmpdir_factory.mktemp("test_registry_from_yml")) / "test.yml")
+    with open(fn, "w") as f:
+        f.write("""
+            torch.unsqueeze:
+                params: {input: torch.Tensor, dim: int}
+                returns: {my_name: torch.Tensor}
+    """)
+    core.register_components_from_yml(str(fn))
+    comp = limbus.components.torch.unsqueeze("test")
+    inp = Params()
+    inp.declare("input", torch.Tensor, torch.randn(2, 3))
+    inp.declare("dim", int, 2)
+    comp(inp)
+    assert comp.outputs.get_param("my_name").shape == torch.Size([2, 3, 1])
+
+
+def test_registry_from_yml_with_args_default_value(tmpdir_factory):
+    fn = str(Path(tmpdir_factory.mktemp("test_registry_from_yml")) / "test.yml")
+    with open(fn, "w") as f:
+        f.write("""
+            torch.unsqueeze:
+                params: {input: torch.Tensor, dim: int = 2}
+                returns: {out: torch.Tensor}
+    """)
+    core.register_components_from_yml(str(fn))
+    comp = limbus.components.torch.unsqueeze("test")
+    inp = Params()
+    inp.declare("input", torch.Tensor, torch.randn(2, 3))
+    inp.declare("dim", int, comp.inputs.dim)  # TODO: default params shouldn't be explicitly declared
+    comp(inp)
+    assert comp.outputs.get_param("out").shape == torch.Size([2, 3, 1])
+
+
+def test_registry_from_yml_with_signature_idx(tmpdir_factory):
+    fn = str(Path(tmpdir_factory.mktemp("test_registry_from_yml")) / "test.yml")
+    with open(fn, "w") as f:
+        f.write("""
+            torch.unsqueeze:
+                idx: 0
+    """)
+    core.register_components_from_yml(str(fn))
+    comp = limbus.components.torch.unsqueeze("test")
+    inp = Params()
+    inp.declare("input", torch.Tensor, torch.randn(2, 3))
+    inp.declare("dim", int, 2)
+    comp(inp)
+    assert comp.outputs.get_param("out").shape == torch.Size([2, 3, 1])
+
+
+def test_registry_from_yml_only_name(tmpdir_factory):
+    fn = str(Path(tmpdir_factory.mktemp("test_registry_from_yml")) / "test.yml")
+    with open(fn, "w") as f:
+        f.write("""
+            torch.unsqueeze:
+    """)
+    core.register_components_from_yml(str(fn))
+    comp = limbus.components.torch.unsqueeze("test")
+    inp = Params()
+    inp.declare("input", torch.Tensor, torch.randn(2, 3))
+    inp.declare("dim", int, 2)
+    comp(inp)
+    assert comp.outputs.get_param("out").shape == torch.Size([2, 3, 1])
