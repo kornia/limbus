2873e60038b3a96c571c7ab41417585e5777fdd0
Luis Ferraz <luisferrazc@gmail.com>
2022-05-01 01:19:03 +0200

    allow to deregister components. Now the filenames of the default modules with components must coincide with the modules name.

--- limbus/components/__init__.py
+++ limbus/components/__init__.py
@@ -2,13 +2,21 @@
 from pathlib import Path
 
 from limbus.core import factory
-# this line needs to be at the top of the file
+# this line needs to be at the top of the file. Otherwise, the components will not be registered.
 factory.COMP_GLOBALS = globals()
-from limbus.core import Component  # noqa: E402
-from limbus.core import register_components_from_yml, register_component  # noqa: E402
-from limbus.components import base  # noqa: E402
 
-# register all components in the default ymls
-DEFAULT_COMPONENT_FILES = ["pytorch.yml", "kornia.yml"]
+from limbus.core import Component  # noqa: E402
+from limbus.core import register_components_from_yml, register_components_from_path, register_component  # noqa: E402
+
+# IMPORTANT NOTE: In order to be able to deregister components with safety we need to list all the imported modules.
+IMPORTED_MODULES = ["factory"]
+
+# register all the default components (the name of the file must be the name of the module)
+# check the deregister_all_components function to see how this var is used.
+DEFAULT_COMPONENT_FILES = ["torch.yml", "kornia.yml", "base.py"]
 for comp_file in DEFAULT_COMPONENT_FILES:
-    register_components_from_yml(str(Path(__file__).parent / comp_file))
+    path = Path(__file__).parent / comp_file
+    if path.suffix == ".py":
+        register_components_from_path(str(path))
+    else:
+        register_components_from_yml(str(path))
--- limbus/core/__init__.py
+++ limbus/core/__init__.py
@@ -1,7 +1,8 @@
 from limbus.core.component import Component, ComponentState, Params, NoValue, Param
 from limbus.core.manager import DefaultParam, Pipeline
 from limbus.core.factory import (ComponentDefinition, register_components, register_component,
-                                 register_components_from_yml, register_components_from_path)
+                                 register_components_from_yml, register_components_from_path,
+                                 deregister_all_components)
 
 __all__ = [
     "Pipeline",
@@ -14,5 +15,6 @@ __all__ = [
     "register_component",
     "register_components_from_yml",
     "register_components_from_path",
+    "deregister_all_components",
     "ComponentDefinition",
     "NoValue"]
