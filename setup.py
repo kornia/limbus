980412c88fe4e9b8ea0fa5223d11fcd987edc4e1
Luis Ferraz <luisferrazc@gmail.com>
2023-02-05 19:29:23 +0100

    add limbus-components dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='limbus',
       version='0.1.0.dev',
       description='High level interface to create Pytorch Graphs.',
-      author='Kornia.org',
+      author='Luis Ferraz',
       url='https://github.com/kornia/limbus',
       install_requires=[
           'torch',
@@ -11,7 +11,6 @@ setup(name='limbus',
           'visdom',
           'typeguard',
           'kornia',
-          'pyyaml',
           'opencv-python<4.7'  # 4.7 doesn't work in my computer
       ],
       extras_require={
@@ -26,7 +25,9 @@ setup(name='limbus',
               'pydocstyle',
               'flake8<5.0.0',  # last versions of flake8 are not compatible with pytest-flake8==1.1.1 (lastest version)
               'pep8-naming',
-              'types-PyYAML'
+          ],
+          'components': [
+              'limbus-components@git+https://git@github.com/kornia/limbus-components.git'
           ]
       },
       packages=find_packages(where='.'),
