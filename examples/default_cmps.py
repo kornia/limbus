98363cee2cdd3624a665f83c773c88c53080d925
Luis Ferraz <luisferrazc@gmail.com>
2023-03-03 21:43:26 +0100

    improve example with default components

--- examples/default_cmps.py
+++ examples/default_cmps.py
@@ -1,5 +1,7 @@
 """Basic example with predefined cmps."""
 import asyncio
+from sys import version_info
+import copy
 
 import torch
 
@@ -25,15 +27,24 @@ t1.outputs.out >> stack.inputs.tensors.select(0)
 t2.outputs.out >> stack.inputs.tensors.select(1)
 stack.outputs.tensor >> show.inputs.inp
 
-# create the pipeline and add its nodes
-pipeline = Pipeline()
-pipeline.add_nodes([c1, t1, t2, stack, show])
+USING_PIPELINE = True
+if USING_PIPELINE:
+    # run your pipeline (only one iteration, note that this pipeline can run forever)
+    print("Run with pipeline:")
+    # create the pipeline and add its nodes
+    pipeline = Pipeline()
+    pipeline.add_nodes([c1, t1, t2, stack, show])
+    pipeline.run(1)
+else:
+    # run 1 iteration using the asyncio loop
+    print("Run with loop:")
+    async def f():  # noqa: D103
+        await asyncio.gather(c1(), t1(), t2(), stack(), show())
 
-# run your pipeline (only one iteration, note that this pipeline can run forever)
-print("Run with pipeline:")
-pipeline.run(1)
-
-# run 1 iteration using the asyncio loop
-print("Run with loop:")
-loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.gather(c1(), t1(), t2(), stack(), show()))
+    if version_info.minor < 10:
+        # for python <3.10 the loop must be run in this way to avoid creating a new loop.
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(f())
+    elif version_info.minor >= 10:
+        # for python >=3.10 the loop should be run in this way.
+        asyncio.run(f())
